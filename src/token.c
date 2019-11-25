98893b492d6a4e6dfbb482ce63c62ba77cc6c008
c45y <git@c45y.com>
2019-11-25 19:08:06 +1000

    comments & indenting

--- src/token.c
+++ src/token.c
@@ -9,14 +9,25 @@
 
 #include "token.h"
 
-
+/** Explicit wrapper to memcmp that checks equal length
+ *
+ *  \param[in]  str1  string to compare
+ *  \param[in]  str2  string to compare
+ *
+ *  \return  true if strings are same length and content, otherwise false
+ */
 bool streq(const char *str1, const char *str2) {
     if (strlen(str1) != strlen(str2))
         return false;
     return (bool) (memcmp(str1, str2, strlen(str1)) == 0);
 }
 
-
+/** Check a string against known keywords
+ *
+ *  \param[in]  token  string to compare [N]IF
+ *
+ *  \return  type of token found, defaults to STR unless explicit match
+ */
 TokenType gettokentype(const char *token) {
     if (streq("IF", token)) {
         return IF_EQ;
@@ -26,7 +37,13 @@ TokenType gettokentype(const char *token) {
     return STR;
 }
 
-
+/** Process the FILE stream into a doubly linked list of Tokens
+ *
+ *  \param[in]  stream  pointer file we're working with, is mutated
+ *  \param[in]  tok     Token to append the next parsed element to
+ *
+ *  \return  pointer to malloc'd Token, NULL we're done parsing
+ */
 Token *gettoken(FILE *stream, Token *tok) {
     char c;
     bool in_quotes = false;
@@ -103,7 +120,13 @@ Token *gettoken(FILE *stream, Token *tok) {
     return NULL;
 }
 
-
+/** Follow the list forwards until TokenType specified is found
+ *
+ *  \param[in]  head  pointer to starting location in the list
+ *  \param[in]  type  TokenType we're searching for
+ *
+ *  \return  pointer to matching Token, NULL if not found
+ */
 Token *token_find_next_of(Token *head, TokenType type) {
     Token *t = head;
     while (t != NULL) {
@@ -114,6 +137,12 @@ Token *token_find_next_of(Token *head, TokenType type) {
     return NULL;
 }
 
+/** Follow the list backwards until we hit an [N]IF token type
+ *
+ *  \param[in]  head  pointer to starting location in the list
+ *
+ *  \return  pointer to Token conditional, NULL if not found
+ */
 Token *token_find_last_conditional(Token *head) {
     Token *t = head;
     while (t != NULL) {
@@ -124,9 +153,12 @@ Token *token_find_last_conditional(Token *head) {
     return NULL;
 }
 
+/** Recursively free elements from the list
+ *
+ *  \param[in] temp  pointer to a list member, preferrably the head
+ */
 void token_follow_free(Token *temp) {
     Token *t;
-
     while (temp != NULL) {
         t = temp;
         temp = temp->next;
